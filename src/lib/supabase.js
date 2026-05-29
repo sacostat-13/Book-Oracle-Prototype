805dbe39f334e991393f266e8ac1efe409a009ad
sacostat-13 <mandala.xiii@gmail.com>
2026-05-29 16:40:29 -0600

    Fix name of env on js file

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import {
+  createClient
+} from '@supabase/supabase-js';
 
-const url = import.meta.env.VITE_SUPABASE_URL;
-const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const url =
+  import.meta.env.VITE_SUPABASE_URL;
+const anonKey =
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 if (!url || !anonKey) {
   console.warn(
